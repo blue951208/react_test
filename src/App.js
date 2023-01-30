6fbf2b4b374f431f5e25adcfc4581a33a59dbb59
SonDongHyun <blue951208@naver.com>
2023-01-30 21:34:49 +0900

    초보자를 위한 리액트200제 001~003

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import R003_ImportConponent from "./R003_ImportConponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+        <div>
+          <h1>Start React 200!</h1>
+          <p>HTML 적용하기</p>
+        </div>
+          {/*컴포넌트 추가*/}
+          <R003_ImportConponent></R003_ImportConponent>
+      </>
   );
 }
 
